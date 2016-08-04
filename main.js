b35af4d289a6aaa0306a1501cf014c23cb77eca1
Ajusa Jain <arhamjain@gmail.com>
2016-08-04 11:52:20 -0400

    Creates a json
    Creates output json file, which is then uploaded to firebase by hand

--- main.js
+++ main.js
@@ -1,18 +1,25 @@
 var cheerio = require('cheerio');
 var fs = require('fs');
-var obj = [];
+var firebase = require("firebase");
+firebase.initializeApp({
+    databaseURL: "https://crackbowl-3dd7d.firebaseio.com/",
+});
+var db = firebase.database();
+var ref = db.ref("/questions");
+var request = require('request');
+
+var obj = {};
 var words = ["mention", "is", "either", "before", "or", "accept", "like", "answers", "[", "]", "on", "until", "it", "mentioned", "synonyms", "the", "do", "any", "kind", "of", "mention", "a"];
 var chars = [".", "[", "]", ",", "(", ")", ";", '"'];
 var heading;
-fs.readFile('source.html', 'utf8', function(err, contents) {
-    $ = cheerio.load(contents);
+request("http://www.quinterest.org/php/searchDatabase.php?limit=&info=&categ=All&sub=None&stype=Answer&qtype=Tossups&difficulty=HS&tournamentyear=All", function(error, response, body) {
+    $ = cheerio.load(body);
     $('div.col-md-12').each(function(i, elem) {
         var raw = $(this).find('p').next().text();
         heading = $(this).find('p').find('b').text();
-        heading = heading.slice(heading.indexOf("|")+2,heading.length);
-        if (raw.indexOf("ANSWER:") == -1) {} else {
+        heading = heading.slice(heading.indexOf("|") + 2, heading.length);
+        if (raw.indexOf("ANSWER:") == -1 || raw.split("ANSWER:")[1].trim() == "") {} else {
             var arr = [];
-
             var answer = raw.split("ANSWER:")[1];
             answer = answer.trim();
             var finalObj = {
@@ -20,6 +27,10 @@ fs.readFile('source.html', 'utf8', function(err, contents) {
                 prompts: [],
                 heading: heading,
             }
+            text = heading.split("|");
+            finalObj.level = "HS"
+            finalObj.subject = text[text.length - 1].trim() //biology
+            finalObj.topic = text[text.length - 2].trim() //science
             for (var i = chars.length - 1; i >= 0; i--) {
                 newanswer = answer;
                 do {
@@ -59,10 +70,14 @@ fs.readFile('source.html', 'utf8', function(err, contents) {
                 return array.indexOf(value) == index;
             });
             finalObj.answers = arr;
-            obj.push(finalObj)
+            obj[finalObj.topic] = obj[finalObj.topic] || {};
+            obj[finalObj.topic].list = obj[finalObj.topic].list || [];
+            obj[finalObj.topic].list.push(finalObj)
+            obj[finalObj.topic].count = obj[finalObj.topic].list.length
         }
     });
-    fs.writeFile("output.json", JSON.stringify(obj, null, "\t"), function(err) {
+    fs.writeFile("output.json", JSON.stringify({questions:obj}, null, "\t"), function(err) {
         console.log("The file was saved!");
     });
+    //ref.set(obj);
 });
